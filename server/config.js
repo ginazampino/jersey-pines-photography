704a1d99dc04b72f0830d4f1966fe705f64420b6
Gina Zampino <zampino.gina@gmail.com>
2018-09-24 22:08:32 -0400

    Dockerized configuration.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mysql: {
+        database: 'photosite',
+        host: process.env.MYSQL_HOST || 'localhost',
+        username: process.env.MYSQL_USERNAME || 'root',
+        password: process.env.MYSQL_PASSWORD || ''
+    },
     secret: process.env.SECRET_KEY || 'secret',
     server: {
         port: parseInt(process.env.HTTP_PORT || '8111')
--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
+const { mysql } = require('./config');
 
-const sequelize = new Sequelize('photosite', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(mysql.database, mysql.username, mysql.password, {
+    host: mysql.host,
     dialect: 'mysql',
     operatorsAliases: false,
     pool: {
