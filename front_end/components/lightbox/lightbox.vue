5aee603c136e5504c1fcf242dbd0ec14df4b8cc2
Jesse Colin Hallam <jesse.colin.hallam@gmail.com>
2019-08-03 21:12:51 -0400

    Added EXIF data to lightbox view.

--- front_end/components/lightbox/lightbox.vue
+++ front_end/components/lightbox/lightbox.vue
@@ -13,22 +13,56 @@
         <li class="lightbox-details-title">{{ image.image_title }}</li>
         <li class="lightbox-details-locdate">{{ image.image_location }}, {{ image.image_date }}</li>
         <li class="lightbox-details-note">{{ image.image_note }} All rights reserved.</li>
+        <li class="lightbox-details-exif">{{ exif_info }}</li>
       </ul>
     </div>
     <div class="lightbox">
-      <img class="lightbox-image" v-bind:src="image.image_url + '/lg.jpg'">
+      <img class="lightbox-image" v-bind:src="image.image_url + '/lg.jpg'" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { ExifTagParser } from "../../exif";
 
 export default {
   data: function() {
     return {
       image: []
     };
+  },
+
+  computed: {
+    exif_info() {
+      if (!this.image || !this.image.exif) return null;
+      const parser = new ExifTagParser(this.image.exif);
+      const makeModel = parser.camera_make_and_model();
+      const exposureTime = parser.exposure_time();
+      const focalLength = parser.focal_length();
+      const fstop = parser.fstop();
+      const iso = parser.iso();
+      const lens = parser.lens();
+
+      let result = "";
+      let values = [];
+
+      if (makeModel) {
+        result += makeModel;
+        if (lens) {
+          result += " + " + lens;
+        }
+      }
+
+      if (focalLength) values.push(focalLength);
+      if (fstop) values.push(fstop);
+      if (exposureTime) values.push(exposureTime);
+      if (iso) values.push(iso);
+
+      if (result) result += " @ ";
+      result += values.join(", ");
+      return result;
+    }
   },
 
   created() {
@@ -48,4 +82,14 @@ export default {
     }
   }
 };
+
+function formatFractionalSeconds(value) {
+  if (!value) return "";
+  if (typeof value === "string") value = parseFloat(value);
+  if (value >= 1) {
+    return value.toFixed(1) + "s";
+  } else {
+    return "1/" + (1 / value).toFixed(0);
+  }
+}
 </script>
---
+++ front_end/exif.js
@@ -0,0 +1,87 @@
+/**
+ * This module is used for formatting EXIF tags for user visible sections of the site.
+ * It deals with edge-cases and other strange situations that arise from parsing EXIF
+ * tag data.
+ */
+
+export class ExifTagParser {
+    constructor(exif) {
+        this.exif = exif;
+    }
+
+    camera_make_and_model() {
+        let make = this._camera_make();
+        let model = this._camera_model();
+
+        [make, model] = fixCameraMakeModel(make, model);
+
+        if (make && model) return make + ' ' + model;
+        if (make) return make;
+        if (model) return model;
+    }
+
+    exposure_time() {
+        if (typeof this.exif.exposure_time === 'number') {
+            if (this.exif.exposure_time > 1) {
+                return this.exif.exposure_time.toFixed(1) + 's';
+            } else {
+                return '1/' + (1 / this.exif.exposure_time).toFixed(0);
+            }
+        }
+        if (typeof this.exif.exposure_time === 'string') return this.exif.exposure_time;
+        return null;
+    }
+
+    focal_length() {
+        if (typeof this.exif.focal_length === 'number') return this.exif.focal_length + 'mm';
+        if (typeof this.exif.focal_length === 'string') return this.exif.focal_length;
+        return null;
+    }
+
+    fstop() {
+        if (typeof this.exif.fstop === 'number') return 'f/' + this.exif.fstop;
+        if (typeof this.exif.fstop === 'string') return this.exif.fstop;
+        return null;
+    }
+
+    iso() {
+        if (typeof this.exif.iso === 'number') return 'ISO-' + this.exif.iso;
+        if (typeof this.exif.iso === 'string') return this.exif.iso;
+        return null;
+    }
+
+    lens() {
+        let value = '';
+
+        if (typeof this.exif.lens_make === 'string') value += this.exif.lens_make;
+        if (typeof this.exif.lens_model === 'string') {
+            if (value) value += ' ';
+            value += this.exif.lens_model;
+        }
+
+        return fixLensMakeModel(value);
+    }
+
+    _camera_make() {
+        if (typeof this.exif.camera_make === 'string') return this.exif.camera_make;
+        return null;
+    }
+
+    _camera_model() {
+        if (typeof this.exif.camera_model === 'string') return this.exif.camera_model;
+        return null;
+    }
+}
+
+function fixCameraMakeModel(make, model) {
+    if (make === 'NIKON CORPORATION' && /^NIKON/i.test(model)) {
+        return [null, model];
+    }
+    return [make, model];
+}
+
+function fixLensMakeModel(makeModel) {
+    return makeModel.replace(/\.\d+/g, '').replace(/(\d) +mm/g, function(_, number) {
+        return number + 'mm';
+    });
+}
